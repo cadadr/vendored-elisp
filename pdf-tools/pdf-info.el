46ab21cd87c1947bb4560a1a60d86f5b9096db6c
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2019-11-20 15:36:35 +0300

    pdf-tools/pdf-info.el: simpler way to find epdfinfo
    Sadly, loading ‘pdf-annot’ requires running epdfinfo because some
    defcustom wants to know what types of annotation it supports.

--- pdf-tools/pdf-info.el
+++ pdf-tools/pdf-info.el
@@ -62,28 +62,9 @@
   :group 'pdf-tools)
 
 (defcustom pdf-info-epdfinfo-program
-  (let ((executable (if (eq system-type 'windows-nt)
-                        "epdfinfo.exe" "epdfinfo"))
-        (default-directory
-          (or (and load-file-name
-                   (file-name-directory load-file-name))
-              default-directory)))
-    (cl-labels ((try-directory (directory)
-                  (and (file-directory-p directory)
-                       (file-executable-p (expand-file-name executable directory))
-                       (expand-file-name executable directory))))
-      (or (executable-find executable)
-          ;; This works if epdfinfo is in the same place as emacs and
-          ;; the editor was started with an absolute path, i.e. it is
-          ;; ment for Windows/Msys2.
-          (and (stringp (car-safe command-line-args))
-               (file-name-directory (car command-line-args))
-               (try-directory
-                (file-name-directory (car command-line-args))))
-          ;; If we are running directly from the git repo.
-          (try-directory (expand-file-name "../server"))
-          ;; Fall back to epdfinfo in the directory of this file.
-          (expand-file-name executable))))
+  (expand-file-name "server/epdfinfo"
+		    (file-name-directory (or load-file-name
+					     buffer-file-name)))
   "Filename of the epdfinfo executable."
   :group 'pdf-info
   :type 'file)
