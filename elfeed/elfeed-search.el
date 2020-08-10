216a8aaf8052c12db444c8e4b3f3fa337969e331
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2020-08-10 03:09:36 +0300

    Elfeed 3.3.0

--- elfeed/elfeed-search.el
+++ elfeed/elfeed-search.el
@@ -8,6 +8,7 @@
 (require 'browse-url)
 (require 'wid-edit) ; widget-inactive face
 (require 'bookmark)
+(bookmark-maybe-load-default-file)
 
 (require 'elfeed)
 (require 'elfeed-db)
@@ -53,6 +54,13 @@ during live filter editing, but the results be will correct when
 live filter editing is exited."
   :group 'elfeed
   :type '(choice function (const nil)))
+
+(defcustom elfeed-search-remain-on-entry nil
+  "When non-nil, keep point at entry after performing a command.
+
+When nil, move to next entry."
+  :group 'elfeed
+  :type 'boolean)
 
 (defcustom elfeed-search-clipboard-type 'PRIMARY
   "Selects the clipboard `elfeed-search-yank' should use.
@@ -107,24 +115,39 @@ When live editing the filter, it is bound to :live.")
   (elfeed-db-save)
   (quit-window))
 
+(defun elfeed-search-last-entry ()
+  "Place point on first entry."
+  (interactive)
+  (setf (point) (point-max))
+  (forward-line -2))
+
+(defun elfeed-search-first-entry ()
+  "Place point on last entry."
+  (interactive)
+  (setf (point) (point-min)))
+
 (defvar elfeed-search-mode-map
   (let ((map (make-sparse-keymap)))
     (prog1 map
       (suppress-keymap map)
-      (define-key map "q" 'elfeed-search-quit-window)
-      (define-key map "g" 'elfeed-search-update--force)
-      (define-key map "G" 'elfeed-search-fetch)
-      (define-key map (kbd "RET") 'elfeed-search-show-entry)
-      (define-key map "s" 'elfeed-search-live-filter)
-      (define-key map "S" 'elfeed-search-set-filter)
-      (define-key map "b" 'elfeed-search-browse-url)
-      (define-key map "y" 'elfeed-search-yank)
-      (define-key map "u" 'elfeed-search-tag-all-unread)
-      (define-key map "r" 'elfeed-search-untag-all-unread)
-      (define-key map "n" 'next-line)
-      (define-key map "p" 'previous-line)
-      (define-key map "+" 'elfeed-search-tag-all)
-      (define-key map "-" 'elfeed-search-untag-all)))
+      (define-key map "h" #'describe-mode)
+      (define-key map "q" #'elfeed-search-quit-window)
+      (define-key map "g" #'elfeed-search-update--force)
+      (define-key map "G" #'elfeed-search-fetch)
+      (define-key map (kbd "RET") #'elfeed-search-show-entry)
+      (define-key map "s" #'elfeed-search-live-filter)
+      (define-key map "S" #'elfeed-search-set-filter)
+      (define-key map "c" #'elfeed-search-clear-filter)
+      (define-key map "b" #'elfeed-search-browse-url)
+      (define-key map "y" #'elfeed-search-yank)
+      (define-key map "u" #'elfeed-search-tag-all-unread)
+      (define-key map "r" #'elfeed-search-untag-all-unread)
+      (define-key map "n" #'next-line)
+      (define-key map "p" #'previous-line)
+      (define-key map "+" #'elfeed-search-tag-all)
+      (define-key map "-" #'elfeed-search-untag-all)
+      (define-key map "<" #'elfeed-search-first-entry)
+      (define-key map ">" #'elfeed-search-last-entry)))
   "Keymap for elfeed-search-mode.")
 
 (defun elfeed-search--intro-header ()
@@ -180,7 +203,7 @@ When live editing the filter, it is bound to :live.")
    ((> (elfeed-queue-count-total) 0)
     (let ((total (elfeed-queue-count-total))
           (in-process (elfeed-queue-count-active)))
-      (format "%d feeds pending, %d in process ..."
+      (format "%d jobs pending, %d active..."
               (- total in-process) in-process)))
    ((let* ((db-time (seconds-to-time (elfeed-db-last-update)))
            (update (format-time-string "%Y-%m-%d %H:%M" db-time))
@@ -352,6 +375,7 @@ The customization `elfeed-search-date-format' sets the formatting."
   "Parse the elements of a search filter into a plist."
   (let ((must-have ())
         (must-not-have ())
+        (before nil)
         (after nil)
         (matches ())
         (not-matches ())
@@ -368,16 +392,26 @@ The customization `elfeed-search-date-format' sets the formatting."
                    (let ((symbol (intern (substring element 1))))
                      (unless (eq '## symbol)
                        (push symbol must-not-have))))
-                  (?@ (setf after (elfeed-time-duration (substring element 1))))
+                  (?@ (cl-multiple-value-bind (a b)
+                          (split-string (substring element 1) "--")
+                        (let ((duration-a (elfeed-time-duration a))
+                              (duration-b (and b (elfeed-time-duration b))))
+                          (when (and duration-b (> duration-b duration-a))
+                            (cl-rotatef duration-a duration-b))
+                          (when duration-b (setf before duration-b))
+                          (setf after duration-a))))
                   (?! (let ((re (substring element 1)))
                         (when (elfeed-valid-regexp-p re)
                           (push re not-matches))))
                   (?# (setf limit (string-to-number (substring element 1))))
-                  (?= (let ((url (substring element 1)))
-                        (push url feeds)))
+                  (?= (let ((re (substring element 1)))
+                        (when (elfeed-valid-regexp-p re)
+                          (push re feeds))))
                   (otherwise (when (elfeed-valid-regexp-p element)
                                (push element matches)))))
-    `(,@(when after
+    `(,@(when before
+          (list :before before))
+      ,@(when after
           (list :after after))
       ,@(when must-have
           (list :must-have must-have))
@@ -392,7 +426,7 @@ The customization `elfeed-search-date-format' sets the formatting."
       ,@(when feeds
           (list :feeds feeds)))))
 
-(defun elfeed-search--recover-units (seconds)
+(defun elfeed-search--recover-time (seconds)
   "Pick a reasonable filter representation for SECONDS."
   (let ((units '((60   1 "minute")
                  (60   1 "hour")
@@ -409,7 +443,15 @@ The customization `elfeed-search-date-format' sets the formatting."
              do (setf name unit
                       value next-value))
     (let ((count (format "%.4g" value)))
-      (format "@%s-%s%s-ago" count name (if (equal count "1") "" "s")))))
+      (format "%s-%s%s-ago" count name (if (equal count "1") "" "s")))))
+
+(defun elfeed-search--recover-units (after-seconds &optional before-seconds)
+  "Stringify the age or optionally the date range specified by
+AFTER-SECONDS and BEFORE-SECONDS."
+  (apply 'concat "@"
+          (elfeed-search--recover-time after-seconds)
+          (when before-seconds
+            (list "--"(elfeed-search--recover-time before-seconds)))))
 
 (defun elfeed-search-unparse-filter (filter)
   "Inverse of `elfeed-search-parse-filter', returning a string.
@@ -418,6 +460,7 @@ The time (@n-units-ago) filter may not exactly match the
 original, but will be equal in its effect."
   (let ((output ()))
     (let ((after (plist-get filter :after))
+          (before (plist-get filter :before))
           (must-have (plist-get filter :must-have))
           (must-not-have (plist-get filter :must-not-have))
           (matches (plist-get filter :matches))
@@ -425,7 +468,7 @@ original, but will be equal in its effect."
           (limit (plist-get filter :limit))
           (feeds (plist-get filter :feeds)))
       (when after
-        (push (elfeed-search--recover-units after) output))
+        (push (elfeed-search--recover-units after before) output))
       (dolist (tag must-have)
         (push (format "+%S" tag) output))
       (dolist (tag must-not-have)
@@ -492,6 +535,7 @@ This function must *only* be called within the body of
 Executing a filter in bytecode form is generally faster than
 \"interpreting\" the filter with `elfeed-search-filter'."
   (let ((after (plist-get filter :after))
+        (before (plist-get filter :before))
         (must-have (plist-get filter :must-have))
         (must-not-have (plist-get filter :must-not-have))
         (matches (plist-get filter :matches))
@@ -541,7 +585,9 @@ Executing a filter in bytecode form is generally faster than
                   `((or ,@(cl-loop
                            for regex in feeds
                            collect `(string-match-p ,regex feed-id)
-                           collect `(string-match-p ,regex feed-title))))))))))
+                           collect `(string-match-p ,regex feed-title)))))
+              ,@(when before
+                  `((> age ,before))))))))
 
 (defun elfeed-search--prompt (current)
   "Prompt for a new filter, starting with CURRENT."
@@ -552,6 +598,12 @@ Executing a filter in bytecode form is generally faster than
        current
      (concat current " "))
    nil nil 'elfeed-search-filter-history))
+
+(defun elfeed-search-clear-filter ()
+  "Reset the search filter to the default value of `elfeed-search-filter'."
+  (interactive)
+  (setf elfeed-search-filter (default-value 'elfeed-search-filter))
+  (elfeed-search-update--force))
 
 (defun elfeed-search-set-filter (new-filter)
   "Set a new search filter for the elfeed-search buffer.
@@ -565,12 +617,23 @@ Any component beginning with a + or - is treated as a tag. If +
 the tag must be present on the entry. If - the tag must *not* be
 present on the entry. Ex. \"+unread\" or \"+unread -comic\".
 
-Any component beginning with an @ is an age limit. No posts older
-than this are allowed. Ex. \"@3-days-ago\" or \"@1-year-old\".
+Any component beginning with an @ is an age limit or an age
+range. If a limit, no posts older than this are allowed. If a
+range, posts dates have to be inbetween the specified date
+range. Examples:
+- \"@3-days-ago\"
+- \"@1-year-old\"
+- \"@2019-06-24\"
+- \"@2019-06-24--2019-06-24\"
+- \"@5-days-ago--1-day-ago\"
 
 Any component beginning with a # is an entry count maximum. The
 number following # determines the maxiumum number of entries
 to be shown (descending by date). Ex. \"#20\" or \"#100\".
+
+Any component beginning with a = is a regular expression matching
+the entry's feed (title or URL). Only entries belonging to a feed
+that match at least one of the = expressions will be shown.
 
 Every other space-seperated element is treated like a regular
 expression, matching against entry link, title, and feed title."
@@ -709,7 +772,8 @@ browser defined by `browse-url-generic-program'."
                     (browse-url-generic it)
                   (browse-url it)))
     (mapc #'elfeed-search-update-entry entries)
-    (unless (use-region-p) (forward-line))))
+    (unless (or elfeed-search-remain-on-entry (use-region-p))
+      (forward-line))))
 
 (defun elfeed-search-yank ()
   "Copy the selected feed items to clipboard and kill-ring."
@@ -726,7 +790,8 @@ browser defined by `browse-url-generic-program'."
           (x-set-selection elfeed-search-clipboard-type links-str)))
       (message "Copied: %s" links-str)
       (mapc #'elfeed-search-update-entry entries)
-      (unless (use-region-p) (forward-line)))))
+      (unless (or elfeed-search-remain-on-entry (use-region-p))
+        (forward-line)))))
 
 (defun elfeed-search-tag-all (tag)
   "Apply TAG to all selected entries."
@@ -734,7 +799,8 @@ browser defined by `browse-url-generic-program'."
   (let ((entries (elfeed-search-selected)))
     (elfeed-tag entries tag)
     (mapc #'elfeed-search-update-entry entries)
-    (unless (use-region-p) (forward-line))))
+    (unless (or elfeed-search-remain-on-entry (use-region-p))
+      (forward-line))))
 
 (defun elfeed-search-untag-all (tag)
   "Remove TAG from all selected entries."
@@ -742,7 +808,8 @@ browser defined by `browse-url-generic-program'."
   (let ((entries (elfeed-search-selected)))
     (elfeed-untag entries tag)
     (mapc #'elfeed-search-update-entry entries)
-    (unless (use-region-p) (forward-line))))
+    (unless (or elfeed-search-remain-on-entry (use-region-p))
+      (forward-line))))
 
 (defun elfeed-search-toggle-all (tag)
   "Toggle TAG on all selected entries."
@@ -755,7 +822,8 @@ browser defined by `browse-url-generic-program'."
     (elfeed-tag entries-tag tag)
     (elfeed-untag entries-untag tag)
     (mapc #'elfeed-search-update-entry entries)
-    (unless (use-region-p) (forward-line))))
+    (unless (or elfeed-search-remain-on-entry (use-region-p))
+      (forward-line))))
 
 (defun elfeed-search-show-entry (entry)
   "Display the currently selected item in a buffer."
@@ -764,7 +832,7 @@ browser defined by `browse-url-generic-program'."
   (when (elfeed-entry-p entry)
     (elfeed-untag entry 'unread)
     (elfeed-search-update-entry entry)
-    (forward-line)
+    (unless elfeed-search-remain-on-entry (forward-line))
     (elfeed-show-entry entry)))
 
 (defun elfeed-search-set-entry-title (title)
